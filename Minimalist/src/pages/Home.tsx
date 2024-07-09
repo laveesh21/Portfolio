01930d2799fe4f0e5b45d8eceaaadc83727c596e
laveesh21 <laveeshtomar.royalking@gmail.com>
2024-07-09 23:00:38 +0530

    New portfolio Minimalist ajdded
    Second portfolio added with dark theme.
    Added Navbar

---
+++ Minimalist/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className='h-dvh w-full bg-zinc-950'>Home</div>
+    </>
+  )
+}
+
+export default Home
